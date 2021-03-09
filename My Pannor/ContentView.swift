8b90d67e8fa15b44dd86a22ab709ec83b00227b6
octavian-ilie <octavian.ilie@icloud.com>
2021-03-09 22:28:25 +0100

    Initial Commit

---
+++ My Pannor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  My Pannor
+//
+//  Created by Octavian Mihuț Ilie on 09/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("My Pannor")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ My Pannor/My_PannorApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_PannorApp.swift
+//  My Pannor
+//
+//  Created by Octavian Mihuț Ilie on 09/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct My_PannorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
